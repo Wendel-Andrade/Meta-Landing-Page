ef3e2e14628c18eef2775f022f1c0befb7391e83
XxWendel-xX <XxWendel-xX>
2022-04-17 00:26:43 -0300

    main structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './pages/Main';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import GlobalStyle from './styles/global';
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <GlobalStyle />
+    <Main />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
